676405e7e7eb20f237f0ec6427b8d7827bb55e77
griffsking <griffking0325@hotmail.com>
2022-05-03 13:56:57 -0400

    The actual important stuff

---
+++ assignment5/server.js
@@ -0,0 +1,51 @@
+const { setDefaultResultOrder } = require('dns');
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+
+app.get('/students', function (req, res) {
+    let dataFile = require('./data.json');
+    let stringData = JSON.stringify(dataFile.students, null, 2);
+    res.end( stringData );
+ })
+
+ app.get('/students/:Id', function (req, res) {
+    let dataFile = require('./data.json');
+    for (let i = 0; i < dataFile.students.length; i++) {
+        if (dataFile.students[i].studentid == req.params.Id) {
+            let stringData = JSON.stringify(dataFile.students[i], null, 2);
+            res.end( stringData );
+        }
+    }
+ })
+
+ app.get('/students/:Id/classes', function (req, res) {
+    let dataFile = require('./data.json');
+    for (let i = 0; i < dataFile.students.length; i++) {
+        if (dataFile.students[i].studentid == req.params.Id) {
+            let stringData = JSON.stringify(dataFile.students[i].classes, null, 2);
+            res.end( stringData );
+        }
+    }
+ })
+
+ app.get('/students/:Id/classes/:Courseno', function (req, res) {
+    let dataFile = require('./data.json');
+    for (let i = 0; i < dataFile.students.length; i++) {
+        if (dataFile.students[i].studentid == req.params.Id) {
+            for (let o = 0; o < dataFile.students[i].classes.length; o++) {
+                if (dataFile.students[i].classes[o].courseno == req.params.Courseno) {
+                let stringData = JSON.stringify(dataFile.students[i].classes[o], null, 2);
+                res.end( stringData );
+                }
+            }
+        }
+    }
+ })
+
+var server = app.listen(3000, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
