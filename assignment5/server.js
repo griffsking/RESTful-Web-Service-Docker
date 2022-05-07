1ff17112fc314238a7bf68bca5e55834bf21d7f9
griffsking <griffking0325@hotmail.com>
2022-05-06 23:53:07 -0400

    Updated to include buttons

--- assignment5/server.js
+++ assignment5/server.js
@@ -5,9 +5,34 @@ var fs = require("fs");
 var port = 3000;
 
 
+/*
+function Home() {
+    return (
+        <div className="Home">
+          <div class="header">
+             <h1 style={{textAlign: "center"}}>Home</h1>
+            </div>
+          </div>
+      );
+    }
+
+export default Home;
+*/
+/*
+const MyComponent = () => { 
+  return React.createElement("div", null, React.createElement("p", null, "This is not HTML...!"), React.createElement("p", null, "Wait...Is this JavaScript?"), React.createElement("p", null, "No, What the hell is this?"), React.createElement("p", null, "This is JSX.")); 
+}; 
+*/
+
 app.get('/students', function (req, res) {
     let dataFile = require('./data.json');
     let stringData = JSON.stringify(dataFile.students, null, 2);
+    res.write('<h1>Student Data<h1>');
+    res.write('<button><a href="http://localhost:3000/students/127" class="button">Student Data for Charles</a></button>');
+    res.write('<button><a href="http://localhost:3000/students/280" class="button">Student Data for Eric</a></button>');
+    res.write('<button><a href="http://localhost:3000/students/311" class="button">Student Data for Gabriel</a></button>');
+    res.write('<h1 style="font-size:10vw"></h1>');
+    res.write('<p style="font-family:verdana"></p>');
     res.end( stringData );
  })
 
